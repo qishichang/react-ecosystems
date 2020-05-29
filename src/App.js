41c51e63ba9a55d13da0d2a302b91238dc53be57
Echo Qi <qishichang@live.com>
2020-05-30 08:02:28 +1200

    Add sample todos app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import TodoList from './todos/TodoList';
 import './App.css';
 
 const App = () => {
-  return <div className="App">Hello, World!</div>;
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 };
 
 export default hot(module)(App);
