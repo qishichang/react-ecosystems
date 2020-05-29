3cc87f3007614bfc4594753bfe02a835de64e55a
Echo Qi <qishichang@live.com>
2020-05-30 07:39:09 +1200

    Hot reload with react-hot-loader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 import './App.css';
 
 const App = () => {
   return <div className="App">Hello, World!</div>;
 };
 
-export default App;
+export default hot(module)(App);
