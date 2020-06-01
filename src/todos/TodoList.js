18a11087d01f631f70dd2626a13dec31418878fd
Echo Qi <qishichang@live.com>
2020-06-02 00:20:14 +1200

    Add markTodoAsCompleted redux flow

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { removeToDo } from './actions';
+import { removeToDo, markTodoAsCompleted } from './actions';
 import TodoListItem from './TodoListItem';
 import NewTodoForm from './NewTodoForm';
 import './TodoList.css';
 
-const TodoList = ({ todos = [{ text: 'Hello' }], onRemovePressed }) => (
+const TodoList = ({
+  todos = [{ text: 'Hello' }],
+  onRemovePressed,
+  onCompletedPressed,
+}) => (
   <div className="list-wrapper">
     <NewTodoForm />
     {todos.map((todo) => (
-      <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />
+      <TodoListItem
+        todo={todo}
+        onRemovePressed={onRemovePressed}
+        onCompletedPressed={onCompletedPressed}
+      />
     ))}
   </div>
 );
@@ -20,6 +28,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (text) => dispatch(removeToDo(text)),
+  onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todos/actions.js
+++ src/todos/actions.js
@@ -9,3 +9,9 @@ export const removeToDo = (text) => ({
   type: REMOVE_TO_DO,
   payload: { text },
 });
+
+export const MARK_TODO_AS_COMPLETED = 'MARK_TODO_AS_COMPLETED';
+export const markTodoAsCompleted = (text) => ({
+  type: MARK_TODO_AS_COMPLETED,
+  payload: { text },
+});
