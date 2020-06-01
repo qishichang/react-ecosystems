e550f101a56996616e522bea43dcaa1a73147f40
Echo Qi <qishichang@live.com>
2020-06-01 23:55:37 +1200

    Enable redux devtools

--- src/store.js
+++ src/store.js
@@ -17,7 +17,10 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
-  let store = createStore(persistedReducer);
+  let store = createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
   let persistor = persistStore(store);
 
   return {
